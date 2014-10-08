0c32ca4f8dca5ed32786e4afb140f9857dfce8db
Jack <jhsw93@gmail.com>
2014-10-08 03:07:02 -0500

    test compute_C and compute_B for congestion approximator objects

--- sherman_test.py
+++ sherman_test.py
@@ -2,23 +2,68 @@ from __future__ import division
 import graph_util
 import networkx as nx
 import numpy as np
+import numpy.testing as npt
 import numpy.linalg as la
 import sherman
 import unittest
 
 
 class ShermanTest(unittest.TestCase):
+  def test_compute_C(s):
+    g = graph_util.complete_graph(5)
+    graph_util.set_edge_capacity(g, (0, 1), 12)
+    graph_util.set_edge_capacity(g, (0, 2), 13)
+    congestion_approximator = sherman.ShermanMaxFlowConductance(g)
+
+    x = np.ones(g.number_of_edges())
+    x[0] = 2
+    x[1] = 3
+    expected = np.ones(g.number_of_edges())
+    expected[0] = 2 * 12
+    expected[1] = 3 * 13
+    npt.assert_array_equal(expected, congestion_approximator.compute_C(x))
+
+    expected[0] = 2 / 12
+    expected[1] = 3 / 13
+    npt.assert_array_equal(expected, congestion_approximator.compute_Cinv(x))
+
+
+  def test_compute_B(s):
+    g = graph_util.complete_graph(5)
+    congestion_approximator = sherman.ShermanMaxFlowConductance(g)
+
+    x = np.zeros(g.number_of_edges())
+    x[0] = 2
+    x[1] = -1.2
+    x[2] = -0.1
+    expected = np.zeros(g.number_of_nodes())
+    expected[0] = -2 + 1.2 + 0.1
+    expected[1] = 2
+    expected[2] = -1.2
+    expected[3] = -0.1
+    actual_Bx = congestion_approximator.compute_B(x)
+    npt.assert_array_equal(expected, actual_Bx)
+
+    # TODO test BT
+
+
+  def test_compute_R(s):
+    # TODO
+    # TODO test RT
+    return
+
+
   def test_max_flow_on_complete_graphs(s):
     epsilon = 0.1
     p = 0.8
-    for n in range(8, 16):
-      for i in range(10):
-        g = graph_util.diluted_complete_graph(n, p)
-        flow, flow_value = sherman.max_flow(g, 0, 1, epsilon)
-        actual_flow_value, actual_flow = nx.ford_fulkerson(
-            g.to_undirected(), 0, 1)
-        s.assertGreater(flow_value, (1.0 - epsilon) * actual_flow_value)
-        s.assertLess(flow_value, (1.0 + epsilon) * actual_flow_value)
+    n = 10
+    for i in range(100):
+      g = graph_util.diluted_complete_graph(n, p)
+      flow, flow_value = sherman.max_flow(g, 0, 1, epsilon)
+      actual_flow_value, actual_flow = nx.ford_fulkerson(
+          g.to_undirected(), 0, 1)
+      s.assertGreater(flow_value, (1.0 - epsilon) * actual_flow_value)
+      s.assertLess(flow_value, (1.0 + epsilon) * actual_flow_value)
 
 
 if __name__ == '__main__':
