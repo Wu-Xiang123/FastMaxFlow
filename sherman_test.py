c272ddced01099431cc01e05fceba7f69506efac
Jack <jhsw93@gmail.com>
2014-10-20 20:12:23 -0500

    Make sherman_test a bit more comprehensive, testing random graphs of varying sparsity

--- sherman_test.py
+++ sherman_test.py
@@ -55,15 +55,17 @@ class ShermanTest(unittest.TestCase):
 
   def test_max_flow_on_complete_graphs(s):
     epsilon = 0.1
-    p = 0.8
     n = 10
-    for i in range(100):
-      g = graph_util.diluted_complete_graph(n, p)
-      flow, flow_value = sherman.max_flow(g, 0, 1, epsilon)
-      actual_flow_value, actual_flow = nx.ford_fulkerson(
-          g.to_undirected(), 0, 1)
-      s.assertGreater(flow_value, (1.0 - epsilon) * actual_flow_value)
-      s.assertLess(flow_value, (1.0 + epsilon) * actual_flow_value)
+    for p in [0.7, 0.8, 0.9, 1.0]:
+      for i in range(100):
+        g = graph_util.diluted_complete_graph(n, p)
+        if not g.has_edge(0, 1):
+          g.add_edge(0, 1, {'capacity': 1})
+        flow, flow_value = sherman.max_flow(g, 0, 1, epsilon)
+        actual_flow_value, actual_flow = nx.ford_fulkerson(
+            g.to_undirected(), 0, 1)
+        s.assertGreater(flow_value, (1.0 - epsilon) * actual_flow_value)
+        s.assertLess(flow_value, (1.0 + epsilon) * actual_flow_value)
 
 
 if __name__ == '__main__':
